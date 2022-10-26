31cc75c3bea58d35c918b5e472c4790b04d7444c
Robin SHAMSNEJAD <rsham@free.fr>
2022-10-26 11:55:27 +0200

    Fix recursive asm

--- TP01/fact_rec.asm
+++ TP01/fact_rec.asm
@@ -1,11 +1,18 @@
-fact_rec(int):
-        add     a5,zero,a0      # a5 = n
-        addi    a4,zero,1       # will be used for "<= 1" instead of "< 2"
-        addi    a0,zero,1       # a0 = <return value>
-condition:
-        ble     a5,a4,return    # if(n < 2) [aka if(n <= 1)] goto return
-        mul     a0,a0,a5        # else <return value>  = <return value> * n
-        addi    a5,a5,-1        # n = n - 1
-        j       condition       # goto condition
-return:
-        ret                     # return
+fact:   # arg: n in a0, returns n! in a1
+        addi sp, sp, -8 # reserve our stack area
+        sw ra, 0(sp)    # save the return address
+        li t0, 2
+        blt a0, t0, ret_one # 0! and 1! == 1
+        sw a0, 4(sp)    # save our n
+        addi a0, a0, -1
+        jal fact        # call fact (n-1)
+                        # a1 <- fact(n-1)
+        lw t0, 4(sp)    # t0 <- n
+        mul a1, t0, a1  # a1 <- n * fact(n-1)
+        j done
+ret_one:
+        li a1, 1
+done:
+        lw ra, 0(sp)    # restore return address from stack
+        addi sp, sp, 8  # free our stack frame
+        jr ra           # and return
