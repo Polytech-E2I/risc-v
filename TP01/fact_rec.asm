97396409abbdc60cc2a72b85519baac48578098a
Robin SHAMSNEJAD <rsham@free.fr>
2022-11-29 21:11:34 +0100

    Add main

--- TP01/fact_rec.asm
+++ TP01/fact_rec.asm
@@ -1,3 +1,10 @@
+        ### TEST VALUES ###
+        addi a0, zero, 5
+        ### FUNCTION TO TEST ###
+        jal  ra, fact
+        ### PAUSE FOR SIMULATION ###
+        addi zero, zero, 0
+
 fact:   # arg: n in a0, returns n! in a1
         addi    sp, sp, -8      # reserve our stack area
         sw      ra, 0(sp)       # save the return address
@@ -5,11 +12,11 @@ fact:   # arg: n in a0, returns n! in a1
         blt     a0, t0, ret_one # 0! and 1! == 1
         sw      a0, 4(sp)       # save our n
         addi    a0, a0, -1
-        jal     fact            # call fact (n-1)
+        jal     ra, fact        # call fact (n-1)
                                 # a1 <- fact(n-1)
         lw      t0, 4(sp)       # t0 <- n
         mul     a1, t0, a1      # a1 <- n * fact(n-1)
-        j       done
+        jal     zero, done
 ret_one:
         addi    a1,zero,1
 done:
